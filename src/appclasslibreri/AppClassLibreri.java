e9ae37d3f45ed96450891837c4459890e1c8a589
ablancoabalde <ablancoabalde@victoria6.danielcastelao.org>
2018-01-25 10:30:03 +0100

    Modificacion

--- src/appclasslibreri/AppClassLibreri.java
+++ src/appclasslibreri/AppClassLibreri.java
@@ -1,21 +1,12 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package appclasslibreri;
 
-/**
- *
- * @author ablancoabalde
- */
+import com.otros.milibreria.MisMetodos;
+
 public class AppClassLibreri {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println(MisMetodos.otroMetodo(10));
     }
     
 }
